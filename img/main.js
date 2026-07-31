cabf18e124b29c031d5a691d6c21f551f916e33c
Danylo Kovtun <daniakovtun2007@gmail.com>
2026-08-01 02:09:30 +0300

    Домашня js

---
+++ img/main.js
@@ -0,0 +1,20 @@
+let result1 = (5+3)*2-7;
+console.log(result1);
+
+let result2 = (10/2)**2;
+console.log(result2);
+
+let result3 = 2*(4+3)-(8/2);
+console.log(result3);
+
+let result4 = (15%4)+5;
+console.log(result4);
+
+let result5 = 3-(2**4);
+console.log(result5);
+
+let result6 = 2+2*2
+console.log(result6);
+
+let result7 = (2+2)*2;
+console.log(result7);
