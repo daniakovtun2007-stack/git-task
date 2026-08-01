1b0f491ff2b2a1a4daeb1fe3240eb7cec60f9d31
Danylo Kovtun <daniakovtun2007@gmail.com>
2026-08-01 22:13:45 +0300

    Homework

---
+++ img/main2.js
@@ -0,0 +1,9 @@
+let userAgeValue = 18;
+let userNameNickname = "Danya";
+
+// Змінюємо значення
+userAgeValue = 20;
+userNameNickname = "Alex";
+
+console.log(userAgeValue);
+console.log(userNameNickname);
